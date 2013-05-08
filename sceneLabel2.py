09f4dbb210c480b260316f93694a09df1f2a1ddf
Jamie Sherrah <jsherrah@gmail.com>
2013-05-08 12:30:58 -0600

    got to the point of displaying histograms.
    but something is wrong, to do with data formats.
    Going to switch to cv2, basically everything is numpy compatible.

--- sceneLabel2.py
+++ sceneLabel2.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# todo:
+#   found this link too late:
+#      http://opencvpython.blogspot.com/2013/03/histograms-4-back-projection.html
+#   has much more succinct code
+
+# todo: convert this from cv to cv2.  See these links:
+#    http://stackoverflow.com/questions/10417108/what-is-different-between-all-these-opencv-python-interfaces
+#    
 import sys
 import cv
 import maxflow
@@ -9,26 +17,60 @@ from scipy.misc import imread
 import maxflow
 from matplotlib import pyplot as ppl
 
-cvimg = cv.LoadImageM("ship-at-sea.jpg")
-cv.NamedWindow("Input image", 1)
+def imgPlanesAsImgArray( img ):
+    # Split the image out into the RGB channels
+    R = cv.CreateMat( img.rows, img.cols, cv.CV_8UC1 )
+    G = cv.CreateMat( img.rows, img.cols, cv.CV_8UC1 )
+    B = cv.CreateMat( img.rows, img.cols, cv.CV_8UC1 )
+    return [cv.GetImage(z) for z in [R, G, B]]
 
-rectShip = (181,   196,    78,    35)
-rectSea  = ( 10,    48,    55,    93)
-# Draw them
+def extractHist( img ):
+    binSz = [32, 32, 32]
+    rngs = [ [0, 255], [0, 255], [0, 255] ]
+    hist = cv.CreateHist( binSz, cv.CV_HIST_ARRAY, rngs, 1 )
+    cv.CalcHist( imgPlanesAsImgArray(img), hist )
+    return hist
+
+cvimg = cv.LoadImageM("ship-at-sea.jpg")
+cv.NamedWindow("Fg/Bg Seg Example", 1)
+
+rectFg = (181,   196,    78,    35)
+rectBg  = ( 10,    48,    55,    93)
+# Draw them on a separate display image
 dimg = cv.CloneMat(cvimg)
-cv.Rectangle( dimg, rectShip[0:2], \
-                  (rectShip[0]+rectShip[2]-1, rectShip[1]+rectShip[3]-1), \
+cv.Rectangle( dimg, rectFg[0:2], \
+                  (rectFg[0]+rectFg[2]-1, rectFg[1]+rectFg[3]-1), \
                   cv.RGB(255,0,0) )
-cv.Rectangle( dimg, rectSea[0:2], \
-                  (rectSea[0]+rectSea[2]-1, rectSea[1]+rectSea[3]-1), \
+cv.Rectangle( dimg, rectBg[0:2], \
+                  (rectBg[0]+rectBg[2]-1, rectBg[1]+rectBg[3]-1), \
                   cv.RGB(255,0,0) ) 
 
+histFg = extractHist( cv.GetSubRect(cvimg,rectFg) )
+histBg = extractHist( cv.GetSubRect(cvimg,rectBg) )
+# Turn into probability distributions
+cv.NormalizeHist( histFg, 1.0 )
+cv.NormalizeHist( histBg, 1.0 )
+
+# Compute the probability of each pixel belonging to the fg/bg class given
+# histograms (which we treat as probability distributions)
+pImgGivenFg = cv.CreateMat( cvimg.rows, cvimg.cols, cv.CV_8UC1 )
+pImgGivenBg = cv.CreateMat( cvimg.rows, cvimg.cols, cv.CV_8UC1 )
+cv.CalcBackProject( imgPlanesAsImgArray( cvimg ), pImgGivenFg, histFg )
+cv.CalcBackProject( imgPlanesAsImgArray( cvimg ), pImgGivenBg, histBg )
+
+# Display orig
 cv.ShowImage("Input Image", dimg)
+cv.WaitKey(0)
+# now fg,bg probs
+cv.ShowImage("Foreground probs", pImgGivenFg)
+cv.WaitKey(0)
+cv.ShowImage("Background probs", pImgGivenBg)
 cv.WaitKey(0)
 
 #img = imread("ship-at-sea.jpg")
 #ppl.imshow(img)
 #ppl.show()
+
 sys.exit()
 
 # Create the graph.
