108533ae887ff85001a13f179626150a9235f148
AntB <amb.enthusiast@gmail.com>
2013-06-26 20:02:09 -0600

    Refactored FeatureGenerator.py, now generates filterbank response from image (i, j, 17) array

--- FeatureGenerator.py
+++ FeatureGenerator.py
@@ -6,48 +6,33 @@
 #        |__ Laplacian of Gaussians [http://homepages.inf.ed.ac.uk/rbf/HIPR2/log.htm]
 #        |__ 
 #    * Local binary patterns see [http://en.wikipedia.org/wiki/Local_binary_patterns]
-import cv2
-
 import numpy as np
 from numpy import exp
-from numpy import sqrt
-from numpy import pi
-from numpy import mgrid
 
 from scipy import signal
-from scipy.misc import imread
-from scipy.stats import norm
 
-import skimage
-from skimage import color, feature, exposure
+
+from skimage import color, feature, io
 
 
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from pylab import meshgrid
-
-from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
+from pylab import meshgrid, cm, imshow, contour, clabel, colorbar, axis, title, show
 
 
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import matplotlib.pyplot as plt
-
-from DataVisualisation import createKernalWindowRanges, plotKernel
 
 
+from DataVisualisation import createKernalWindowRanges, plotKernel, plotFilterComparison
+
+increment = 1
 
 def create1dRGBColourHistogram(imageRGB, numberBins):
-    
-    # See previous code for example
-    
     # check number of bins is an even number [2, 256]
     bins = np.array([2,4,8,16,32,64,128,256])
-    check = np.array([numberBins])
     
-    if (np.shape(np.intersect1d(bins, check))[0] == 0):
-        numberBins = 32
+    # fail if user-input number of bins is not a permitted value
+    assert numberBins in bins, "User specified number of bins is not one of the permitted values:: " + str(bins)
     
     numColours = np.shape(imageRGB)[2]
     
@@ -91,8 +76,6 @@ def create1dRGBColourHistogram(imageRGB, numberBins):
 
 def create3dRGBColourHistogram(imageRGB, numberBins):
     
-    print "Input image shape == ", np.shape(imageRGB)
-    
     bins = np.array([2,4,8,16,32,64,128,256])
     
     # fail if user-input number of bins is not a permitted value
@@ -104,8 +87,6 @@ def create3dRGBColourHistogram(imageRGB, numberBins):
     numPixels = np.shape(imageRGB[:,:,0])[0] * np.shape(imageRGB[:,:,0])[1]
     
     data = imageRGB.reshape((numPixels, 3))
-    print "Shape of flattened rgb:: ", np.shape(data)
-#     del(imageRGB)
     
     hist, edges = np.histogramdd( data, bins=numberBins, range=[[-0.5,255.5],[-0.5,255.5],[-0.5,255.5]] )
     
@@ -132,61 +113,57 @@ def createImageTextons():
     print "Finish me!"
     
 
-def generateFilterbankResponse(image):
+def generateFilterbankResponse(image, window):
     # See [Object Categorization by Learned Universal Visual Dictionary. Winn, Criminisi & Minka, 2005]
-     
+    
     # convert RGB to CIELab
     image = color.rgb2lab(image)
     image_L = image[:,:,0]
     image_a = image[:,:,1]
     image_b = image[:,:,2]
     
-    # Create filters
-    G1, G2, G3, LoG1, LoG2, LoG3,LoG4, Div1xG2, Div1xG3, Div1yG2, Div1yG3 = createDefaultFilterbank()
+    # Create filters - G1, G2, G3, LoG1, LoG2, LoG3,LoG4, dx_G2, dx_G3, dy_G2, dy_G3
+    filters = createDefaultFilterbank(window)
+    numFilters = np.shape(filters)[0]
+    print "Total number of default filters = " + str(numFilters)
     
     # Apply filters & append result into 17D vector for each pixel as follows:
     # Name          Defn                 CIE channel
     #                                L        a        b
-    # G1            N(0, 1)          yes      yes      yes
-    # G2            N(0, 2)          yes      yes      yes
-    # G3            N(0, 4)          yes      yes      yes
-    # LoG1          Lap(N(0, 1))     yes      no       no
-    # LoG2          Lap(N(0, 1))     yes      no       no
-    # LoG3          Lap(N(0, 1))     yes      no       no
-    # LoG4          Lap(N(0, 1))     yes      no       no
-    # Div1xG2       d/dx(N(0,2))     yes      no       no
-    # Div1xG3       d/dx(N(0,4))     yes      no       no
-    # Div1yG2       d/dy(N(0,2))     yes      no       no
-    # Div1yG3       d/dy(N(0,4))     yes      no       no
+    # G1            N(0, 1)          yes      yes      yes    1
+    # G2            N(0, 2)          yes      yes      yes    2
+    # G3            N(0, 4)          yes      yes      yes    3
+    # LoG1          Lap(N(0, 1))     yes      no       no     4
+    # LoG2          Lap(N(0, 1))     yes      no       no     5
+    # LoG3          Lap(N(0, 1))     yes      no       no     6
+    # LoG4          Lap(N(0, 1))     yes      no       no     7
+    # Div1xG2       d/dx(N(0,2))     yes      no       no     8
+    # Div1xG3       d/dx(N(0,4))     yes      no       no     9
+    # Div1yG2       d/dy(N(0,2))     yes      no       no     10
+    # Div1yG3       d/dy(N(0,4))     yes      no       no     11
+    response = np.array([])
     
-    responseVector = np.array([])
-    
-    G1response1 = signal.convolve2d(image_L, G1, mode='same')
-    G1response2 = signal.convolve2d(image_a, G1, mode='same')
-    G1response3 = signal.convolve2d(image_b, G1, mode='same')
-    
-    G2response1 = signal.convolve2d(image_L, G2, mode='same')
-    G2response2 = signal.convolve2d(image_a, G2, mode='same')
-    G2response3 = signal.convolve2d(image_b, G2, mode='same')
-    
-    G3response1 = signal.convolve2d(image_L, G3, mode='same')
-    G3response2 = signal.convolve2d(image_a, G3, mode='same')
-    G3response3 = signal.convolve2d(image_b, G3, mode='same')
-    
-    LoG1response1 = signal.convolve2d(image_L, LoG1, mode='same')
-    LoG2response1 = signal.convolve2d(image_L, LoG2, mode='same')
-    LoG3response1 = signal.convolve2d(image_L, LoG3, mode='same')
-    LoG4response1 = signal.convolve2d(image_L, LoG4, mode='same')
-    
-    Div1xG2response1 = signal.convolve2d(image_L, Div1xG2, mode='same')
-    Div1xG3response1 = signal.convolve2d(image_L, Div1xG3, mode='same')
-    
-    Div1yG2response1 = signal.convolve2d(image_L, Div1yG2, mode='same')
-    Div1yG3response1 = signal.convolve2d(image_L, Div1yG3, mode='same')
-    
-    
-    print "Finish me!"
-    
+    for filterNum in range(0,numFilters):
+        
+        print "Filter#" + str(filterNum+1) + " = ", np.shape(filters[filterNum])
+        
+        if filterNum == 0:
+            response = signal.convolve2d(image_L, filters[filterNum], mode='same')
+            response = np.dstack((response, signal.convolve2d(image_a, filters[filterNum], mode='same')))
+            response = np.dstack((response, signal.convolve2d(image_b, filters[filterNum], mode='same')))
+            
+        elif filterNum ==1 or filterNum==2:
+            response = np.dstack((response, signal.convolve2d(image_L, filters[filterNum], mode='same')))
+            response = np.dstack((response, signal.convolve2d(image_a, filters[filterNum], mode='same'))) 
+            response = np.dstack((response, signal.convolve2d(image_b, filters[filterNum], mode='same')))
+        
+        else:
+            response = np.dstack((response, signal.convolve2d(image_L, filters[filterNum], mode='same')))
+        
+    print "Size of response data = " + str(np.shape(response))     
+    return response
+
+# util methods for setting up filter bank for texton processing
     
 def createDefaultFilterbank(window):
     # create filterbank as defined in [Object Categorization by Learned Universal Visual Dictionary. Winn, Criminisi & Minka, 2005]
@@ -195,23 +172,28 @@ def createDefaultFilterbank(window):
     # Derivative of Gaussian (x):: Div1xG1 = d/dx N(0,2), Div1xG2=d/dx N(0,4)
     # Derivative of Gaussian (y):  Div1yG1 = d/dy N(0,2), Div1yG2=d/dy N(0,4)
     
-    window = int(window)
-    
-    G1 = gaussian_kernel(window, window, 1, 1)
-    G2 = gaussian_kernel(window, window, 2, 2)
-    G3 = gaussian_kernel(window, window, 4, 4)
+    G1 = gaussian_kernel(window, window, 1)
+    G2 = gaussian_kernel(window, window, 2)
+    G3 = gaussian_kernel(window, window, 4)
     
     # see http://homepages.inf.ed.ac.uk/rbf/HIPR2/log.htm
-    LoG1 = laplacianOfGaussian_kernel(window, window, 1, 1)
-    LoG2 = laplacianOfGaussian_kernel(window, window, 1, 2)
-    LoG3 = laplacianOfGaussian_kernel(window, window, 1, 4)
-    LoG4 = laplacianOfGaussian_kernel(window, window, 1, 8)
+    LoG1 = laplacianOfGaussian_kernel(window, window, 1)
+    LoG2 = laplacianOfGaussian_kernel(window, window, 2)
+    LoG3 = laplacianOfGaussian_kernel(window, window, 4)
+    LoG4 = laplacianOfGaussian_kernel(window, window, 8)
     
-    print "Finish me... I need some validation!"
-    return G1, G2, G3, G3, LoG1, LoG2, LoG3, LoG4
+    dx_G1 = gaussian_1xDerivative_kernel(window, window, 2)
+    dx_G2 = gaussian_1xDerivative_kernel(window, window, 4)
+    
+    dy_G1 = gaussian_1yDerivative_kernel(window, window, 2)
+    dy_G2 = gaussian_1yDerivative_kernel(window, window, 4)
+    
+    print "***I need some validation!***"
+    return np.array([G1, G2, G3, LoG1, LoG2, LoG3, LoG4, dx_G1, dx_G2, dy_G1, dy_G2])
     
 # Some util functions
-    
+
+
 def getGradientMagnitude(gradX, gradY):
     # magnitude of image gradient
     return np.sqrt(gradX**2 + gradY**2)
@@ -221,81 +203,99 @@ def getGradientOrientation(gradX, gradY):
     return np.arctan2(gradX, gradY)
 
 
-
-
-# util methods for setting up filter bank for texton processing
-    
-def createStandardTextureFilterBank():
-    # see http://research.microsoft.com/pubs/67408/criminisi_iccv2005.pdf
-    print "Finish me!"
-
-
-
 # util methods for gaussian_kernel derivatives
 
 def gaussian_kernel(windowX, windowY, sigma):
     """ Returns a normalized 2D (windowX x windowY grid) Gaussian kernel array for convolution"""
-    X,Y = createKernalWindowRanges(windowX, windowY, 0.1)
+    X,Y = createKernalWindowRanges(windowX, windowY, increment)
     
     gKernel = gaussianNormalised(X, 0, sigma) * gaussianNormalised(Y, 0, sigma)
+    gSum = np.sum(gKernel)
     
-    return (gKernel / np.sum(gKernel))
+    if gSum == 0:
+        print "Warning gaussian_kernel:: Not normalising by sum of values, as sum = " + str(gSum)
+        return (gKernel)
+    else:
+        return (gKernel / np.sum(gKernel))
 
 
 def laplacianOfGaussian_kernel(windowX, windowY, sigma):
     """ Returns a normalized 2D (windowX x windowY grid) Laplacian of Gaussian (LoG) kernel array for convolution"""
     # See [http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/MARBLE/low/edges/canny.htm]
-    X, Y = createKernalWindowRanges(windowX, windowY, 0.1)
+    X, Y = createKernalWindowRanges(windowX, windowY, increment)
     
     logKernel = -1 * (1 - ( X**2 + Y**2) ) *  exp (- (X**2 + Y**2) / (2 * sigma))
+    gSum = np.sum(logKernel)
     
-    return (logKernel / np.sum(logKernel))
+    if gSum == 0:
+        print "Warning LoG_kernel:: Not normalising by sum of values, as sum = " + str(gSum)
+        return (logKernel)
+    else:
+        return (logKernel / np.sum(logKernel))
 
 
 def gaussian_1xDerivative_kernel(windowX, windowY, sigma):
     # See [http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/MARBLE/low/edges/canny.htm]
-    X, Y = createKernalWindowRanges(windowX, windowY, 0.1)
+    X, Y = createKernalWindowRanges(windowX, windowY, increment)
     
-    g_dx = gaussianFirstDerivative(X, 0, sigma) * gaussianNormalised(Y, 0, sigma)
-        
-    return (g_dx / np.sum(g_dx))
-
-
+    g_dx_kernel = gaussianFirstDerivative(X, 0, sigma) * gaussianNormalised(Y, 0, sigma)
+    gSum = np.sum(g_dx_kernel)
+    
+    if gSum == 0:
+        print "Warning dx_g_kernel:: Not normalising by sum of values, as sum = " + str(gSum)
+        return (g_dx_kernel)
+    else:
+        return (g_dx_kernel / np.sum(g_dx_kernel))
+    
 
 def gaussian_1yDerivative_kernel(windowX, windowY, sigma):
     # See [http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/MARBLE/low/edges/canny.htm]
-    X, Y = createKernalWindowRanges(windowX, windowY, 0.1)
+    X, Y = createKernalWindowRanges(windowX, windowY, increment)
     
-    g_dy = gaussianFirstDerivative(Y, 0, sigma) * gaussianNormalised(X, 0, sigma)
-        
-    return (g_dy / np.sum(g_dy))
+    g_dy_kernel = gaussianFirstDerivative(Y, 0, sigma) * gaussianNormalised(X, 0, sigma)
+    gSum = np.sum(g_dy_kernel)
+    
+    if gSum == 0:
+        print "Warning dy_g_kernel:: Not normalising by sum of values, as sum = " + str(gSum)
+        return (g_dy_kernel)
+    else:
+        return (g_dy_kernel / np.sum(g_dy_kernel))
 
 
 def gaussianNormalised(data, mu, sigma):
     data = data - mu
     g = exp ( - data**2 / (2*sigma**2) )
-    return (g / np.sum(g))
+    gSum = np.sum(g)
     
+    if gSum == 0:
+        print "Warning gaussianNormalised:: Not normalising by sum of values, as sum = " + str(gSum)
+        return (g)
+    else:
+        return (g / np.sum(g))
     
 def gaussianFirstDerivative(data, mu, sigma):
     data = data - mu
     g = -data * exp(-data**2 / (2*sigma**2))
+    gSum = np.sum(g)
     
-    return (g / np.sum(g))
+    if gSum == 0:
+        print "Warning gaussianFirstDerivative:: Not normalising by sum of values, as sum = " + str(gSum)
+        return (g)
+    else:
+        return (g / np.sum(g))
+
+
 
 
 # File IO utils
+
 def readImageFileRGB(imageFileLocation):    
     """This function takes a (i, j, 3) BGR ndarray as read by opencv, and returns an (i, j, 3) RGB ndarray"""
-    image = cv2.imread(imageFileLocation)
     
-    # Re-stack to RGB
-    b = image[:,:,0]
-    g = image[:,:,1]
-    r = image[:,:,2]
-
-    return np.dstack([r, g, b])
+    image = io.imread(imageFileLocation)
     
+#     return np.dstack([r, g, b])
+    return image
 
 def getGreyscaleImage(imageRGB):
     return color.rgb2gray(imageRGB)
@@ -305,6 +305,8 @@ def getGreyscaleImage(imageRGB):
 # Some simple testing
     
 image = readImageFileRGB("ship-at-sea.jpg");
+
+
 grayImage = color.rgb2gray(image)
 
 # hist, range = create1dRGBColourHistogram(image, 8)
@@ -328,28 +330,34 @@ grayImage = color.rgb2gray(image)
 xWindow = 9
 yWindow = 9
 sigma = 1.4
-xRange, yRange = createKernalWindowRanges(xWindow, yWindow, 0.1)
-# gKernel = gaussian_kernel(xWindow, yWindow, sigma, sigma)
-# print "Gaussian kernel range:: ", np.min(gKernel), np.max(gKernel)
-# plotKernel(xRange, yRange, gKernel, "Gaussian kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
+xRange, yRange = createKernalWindowRanges(xWindow, yWindow, increment)
 
-logKernel = laplacianOfGaussian_kernel(xWindow, yWindow, sigma)
-print "Laplacian of Gaussian kernel range:: ", np.min(logKernel), np.max(logKernel)
-plotKernel(xRange, yRange, logKernel, "LOG kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
+# g_kernel = gaussian_kernel(xWindow, yWindow, sigma)
+# print "Gaussian kernel range:: ", np.min(g_kernel), np.max(g_kernel)
+# plotKernel(xRange, yRange, g_kernel, "Gaussian kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
+# filteredImage = signal.convolve2d(grayImage, g_kernel, mode='same')
+# plotFilterComparison(grayImage, filteredImage)
+#  
+# log_kernel = laplacianOfGaussian_kernel(xWindow, yWindow, sigma)
+# print "Laplacian of Gaussian kernel range:: ", np.min(log_kernel), np.max(log_kernel)
+# plotKernel(xRange, yRange, log_kernel, "LOG kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
+# filteredImage = signal.convolve2d(grayImage, log_kernel, mode='same')
+# plotFilterComparison(grayImage, filteredImage)
+# 
+# g_dx_kernel = gaussian_1xDerivative_kernel(xWindow, yWindow, sigma)
+# print "Gaussian X derivative kernel range:: ", np.min(g_dx_kernel), np.max(g_dx_kernel)
+# plotKernel(xRange, yRange, g_dx_kernel, "G_dx kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
+# filteredImage = signal.convolve2d(grayImage, g_dx_kernel, mode='same')
+# plotFilterComparison(grayImage, filteredImage)
+# 
+# g_dy_kernel = gaussian_1yDerivative_kernel(xWindow, yWindow, sigma)
+# print "Gaussian Y derivative kernel range:: ", np.min(g_dy_kernel), np.max(g_dy_kernel)
+# plotKernel(xRange, yRange, g_dy_kernel, "G_dy kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
+# filteredImage = signal.convolve2d(grayImage, g_dy_kernel, mode='same')
+# plotFilterComparison(grayImage, filteredImage)
 
-g_dx_kernel = gaussian_1xDerivative_kernel(xWindow, yWindow, sigma)
-print "Gaussian X derivative kernel range:: ", np.min(logKernel), np.max(logKernel)
-plotKernel(xRange, yRange, g_dx_kernel, "G_dx kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
 
-g_dy_kernel = gaussian_1yDerivative_kernel(xWindow, yWindow, sigma)
-print "Gaussian Y derivative kernel range:: ", np.min(logKernel), np.max(logKernel)
-plotKernel(xRange, yRange, g_dy_kernel, "G_dy kernel, sigma= + " + str(sigma) + ", window=(" + str(xWindow) + "," + str(yWindow) + ")")
+response = generateFilterbankResponse(image, xWindow)
 
-
-# logImage = signal.convolve2d(grayImage, logKernel, mode='same')
-# gImage = signal.convolve2d(grayImage, gKernel, mode='same')
-# plotFilterComparison(grayImage, gImage)
-
-
-
+print "\nFilter response shape=" + str(np.shape(response))  
 
