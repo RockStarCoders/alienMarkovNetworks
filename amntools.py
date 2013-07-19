85352269eeafa8bc96d6af118a2ed0b0454fb702
Jamie Sherrah <jsherrah@gmail.com>
2013-07-19 12:55:42 -0600

    added gplot matrix clone.  might have bug?

--- amntools.py
+++ amntools.py
@@ -1,4 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import colorsys
+
+def _get_colors(num_colors):
+    colors=[]
+    for i in np.arange(0., 360., 360. / num_colors):
+        hue = i/360.
+        lightness = (50 + np.random.rand() * 10)/100.
+        saturation = (90 + np.random.rand() * 10)/100.
+        colors.append(colorsys.hls_to_rgb(hue, lightness, saturation))
+    return colors
+
 
 def estimateNeighbourRMSPixelDiff(imgRGB, nhoodSz):
     # estimate neighbourhood average diff
@@ -27,3 +39,44 @@ def estimateNeighbourRMSPixelDiff(imgRGB, nhoodSz):
     
     sigsq /= cnt
     return np.sqrt(sigsq)
+
+
+def gplotmatrix( X, labels, msize=5, classColours=None ):
+    assert X.ndim == 2
+    D = X.shape[1]
+    assert type(labels)==list or labels.ndim == 1
+    assert( len(labels) == X.shape[0] )
+    # assume labels are contiguous
+    lmin = np.min(labels)
+    lmax = np.max(labels)
+    plt.clf()
+    if classColours == None:
+        classColours = _get_colors(lmax+1)
+
+    idx = 1
+    for r in range(D):
+        for c in range(D):
+            plt.subplot(D,D,idx)
+            idx += 1
+            x1 = X[:,r]
+            x2 = X[:,c]
+            if r==c:
+                # histogram
+                plt.hist( x1 )
+            else:
+                for l in range(lmin,lmax+1):
+                    #print labels==l
+                    plt.plot( x2[labels==l], x1[labels==l], '.', \
+                                  color=classColours[l],\
+                                  markersize=msize)
+                    #print 'foo'
+                    #plt.waitforbuttonpress()
+                    plt.hold(1)
+                plt.hold(0)
+            plt.grid(1)
+            if c==0:
+                plt.ylabel( str(r) )
+            if r==D-1:
+                plt.xlabel( str(c) )
+
+    
