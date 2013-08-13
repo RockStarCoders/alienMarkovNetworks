4d84e08f0213f5af98bd5c579515cc40632c59d6
Jamie Sherrah <jsherrah@gmail.com>
2013-08-13 16:59:56 -0600

    added graph recovery from superpixels thanks to mr peekaboo

--- SuperPixels.py
+++ SuperPixels.py
@@ -63,6 +63,50 @@ def generateImageWithSuperPixelBoundaries(image, segmentationMask):
     return superPixelImage
 
 
+def make_graph(grid):
+    # get unique labels
+    vertices = np.unique(grid)
+ 
+    # map unique labels to [1,...,num_labels]
+    reverse_dict = dict(zip(vertices,np.arange(len(vertices))))
+    grid = np.array([reverse_dict[x] for x in grid.flat]).reshape(grid.shape)
+   
+    # create edges
+    down = np.c_[grid[:-1, :].ravel(), grid[1:, :].ravel()]
+    right = np.c_[grid[:, :-1].ravel(), grid[:, 1:].ravel()]
+    all_edges = np.vstack([right, down])
+    all_edges = all_edges[all_edges[:, 0] != all_edges[:, 1], :]
+    all_edges = np.sort(all_edges,axis=1)
+    num_vertices = len(vertices)
+    edge_hash = all_edges[:,0] + num_vertices * all_edges[:, 1]
+    # find unique connections
+    edges = np.unique(edge_hash)
+    # undo hashing
+    edges = [[vertices[x%num_vertices],
+              vertices[x/num_vertices]] for x in edges]
+ 
+    return vertices, edges
+
+def show_graph(grid, vertices, edges):
+    # compute region centers:
+    gridx, gridy = np.mgrid[:grid.shape[0], :grid.shape[1]]
+    centers = dict()
+    for v in vertices:
+        centers[v] = [gridy[grid == v].mean(), gridx[grid == v].mean()]
+     
+    # plot labels
+    plt.imshow(grid,interpolation='None')
+     
+    # overlay graph:
+    for edge in edges:
+        plt.plot([centers[edge[0]][0],centers[edge[1]][0]],
+                 [centers[edge[0]][1],centers[edge[1]][1]])
+    
+
+
+###################################
+# tests
+###################################
 
 def testSLIC_lenaRGB(numSuperPixels):
     lenaImg = skimage.data.lena()
