5888a27bb9d958e80cf2f5db4639d9822f2d9eb7
amb-enthusiast <amb.enthusiast@gmail.com>
2013-08-28 13:18:02 -0600

    Edited to provide train, validation and test datasets.  User can specify just train and test through the splitRatio string e.g. 0.7,0.0,0.3 would give 70% training data and 30% testing data for the MSRC dataset.

--- createFeatures.py
+++ createFeatures.py
@@ -6,40 +6,91 @@ import numpy as np
 
 # Usage:
 #
-#   createFeatures.py <MSRC data path> <scaleFrac> <outfileBase> <type=csv|pkl>
+#   createFeatures.py <MSRC data path> <scaleFrac> <splitRatio> <outfileBase> <type=csv|pkl>
 #
 
+
+# Function to take msrc data, create features and labels for superpixels and then save to disk
+def createAndSaveFeatureLabelData(msrcData, outfileBase, dataType, outfileType):
+    outfileFtrs = '%s_%s_ftrs.%s' % ( outfileBase, dataType, outfileType )
+    outfileLabs = '%s_%s_labs.%s' % ( outfileBase, dataType, outfileType )
+    # Check can write these files.
+    f=open(outfileFtrs,'w')
+    f.close()
+    f=open(outfileLabs,'w')
+    f.close()
+    
+    # Edited getSuperPixelTrainingData to allow user-defined data split ratios
+    superPixelData     = SuperPixelClassifier.getSuperPixelData(trainData)
+    
+    superPixelFeatures = superPixelTrainData[0]
+    superPixelLabels   = superPixelTrainData[1].astype(np.int32)
+    
+    assert np.all( np.isfinite( superPixelFeatures ) )
+    
+    # Output
+    if outfileType == 'pkl':
+        pomio.pickleObject( superPixelFeatures, outfileFtrs )
+        pomio.pickleObject( superPixelLabels,   outfileLabs )
+    elif outfileType == 'csv':
+        pomio.writeMatToCSV( superPixelFeatures, outfileFtrs )
+        pomio.writeMatToCSV( superPixelLabels,   outfileLabs )
+    else:
+        assert False, 'unknown output file format ' + outfileType
+
+    print 'Output written to file ', outfileFtrs, ' and ', outfileLabs
+
+
 msrcDataDirectory = sys.argv[1]
-scaleFrac         = float(sys.argv[2])
-outfileBase       = sys.argv[3]
-outfileType       = sys.argv[4]
+scaleFrac         = float(sys.argv[2]) # We aren't using this, assume we want all images
+splitRatio        = sys.argv[3]
+outfileBase       = sys.argv[4]
+outfileType       = sys.argv[5]
 
+trainSplit = 0.0
+cvSplit = 0.0
+testSplit = 0.0
+
+ratios = splitRatio.split(",")
+
+assert (len(ratios) == 2 or len(ratios) == 3) , "You did not specifiy 2 or three data split ratios.  Check input value: " + str(splitRatio)
+
+# parse the ratio string, test it matches the number of splits
+if(len(ratios) == 2):
+    trainSplit = float(ratios[0])
+    testSplit = float(ratios[1])
+    cvSplit = 0.0
+elif(len(ratios) == 3):
+    trainSplit = float(ratios[0])
+    cvSplit = float(ratios[1])
+    testSplit = float(ratios[2])
+
+assert (trainSplit + cvSplit + testSplit) == 1.0 , "Data split values do not add up to 1 : " + str(trainSplit + cvSplit + testSplit) + ".  Check input data: " + str(splitRatio) + " & " + str(ratios)
 assert outfileType in ['csv', 'pkl'], 'Unknown outfile type ' + outfileType
-
-outfileFtrs = '%s_ftrs.%s' % ( outfileBase, outfileType )
-outfileLabs = '%s_labs.%s' % ( outfileBase, outfileType )
-# Check can write these files.
-f=open(outfileFtrs,'w')
-f.close()
-f=open(outfileLabs,'w')
-f.close()
-
 assert 0 <= scaleFrac and scaleFrac <= 1
 
-superPixelData     = SuperPixelClassifier.getSuperPixelTrainingData(msrcDataDirectory, scaleFrac)
-superPixelFeatures = superPixelData[0]
-superPixelLabels   = superPixelData[1].astype(np.int32)
 
-assert np.all( np.isfinite( superPixelFeatures ) )
+# Get train, test and cv datasets
+print "\nSplitting data into sets: train =" , trainSplit , "test =" , testSplit , "cvSplit =" , cvSplit
+[trainData, cvData, testData] = pomio.splitInputDataset_msrcData(pomio.msrc_loadImages(msrcDataDirectory, subset=None) , 1.0 , True, trainSplit , cvSplit , testSplit)
 
-# Output
-if outfileType == 'pkl':
-    pomio.pickleObject( superPixelFeatures, outfileFtrs )
-    pomio.pickleObject( superPixelLabels,   outfileLabs )
-elif outfileType == 'csv':
-    pomio.writeMatToCSV( superPixelFeatures, outfileFtrs )
-    pomio.writeMatToCSV( superPixelLabels,   outfileLabs )
-else:
-    assert False, 'unknown output file format ' + outfileType
+assert trainData != None , "Training data object is null"
+assert len(trainData) > 0 , "Training data contains no data"
 
-print 'Output written to file ', outfileFtrs, ' and ', outfileLabs
+assert testData != None , "Testing data object is null"
+assert len(testData) > 0 , "Testing data contains no data"
+
+
+# Process and persist feature and labels for superpixels in image dataset
+print "Create & save training feature and label superpixel data"
+createAndSaveFeatureLabelData(trainData, 1.0, "train" , outfileType)
+
+print "Create & save test feature and label superpixel data"
+createAndSaveFeatureLabelData(testData, 1.0, "test" , outfileType)
+if(cvData != None and len(cvData) > 0):
+    print ""
+    createAndSaveFeatureLabelData(cvData, 1.0, "crossValid" , outfileType)
+
+
+
+
