611d2d075bb59f3551e3195aa081f03727905446
Jamie Sherrah <jsherrah@gmail.com>
2013-10-02 10:28:19 -0600

    made sceneLabelSuperPixels output an image

--- sceneLabelSuperPixels.py
+++ sceneLabelSuperPixels.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# Usage:  ./sceneLbaelSuperPixels.py <classifierPkl> <imageName> 
-#
 # Example:
 #
 #     ./sceneLabelSuperPixels.py randForestClassifierSP.pkl /home/jamie/data/MSRC_ObjCategImageDatabase_v2/Images/7_3_s.bmp
@@ -22,11 +20,23 @@ import pomio
 import FeatureGenerator
 import slic
 import SuperPixels
+import argparse
+import skimage
 
-# parse args
-clfrFn = sys.argv[1]
-imgFn = sys.argv[2]
+parser = argparse.ArgumentParser(description='Classify image and then apply MRF at the superPixel level.')
+parser.add_argument('clfrFn', type=str, action='store', \
+                        help='filename of pkl or csv superPixel classifier file')
+parser.add_argument('infile', type=str, action='store', \
+                        help='filename of input image to be classified')
+parser.add_argument('--outfile', type=str, action='store', \
+                        help='filename of output image with MRF inferred labels')
+parser.add_argument('--verbose', action='store_true')
+parser.add_argument('--K', type=float, action='store', default=0.1, \
+                        help='Weighting for pairwise potential term in MRF.')
 
+args = parser.parse_args()
+
+K = args.K
 dointeract = 1
 dbgMode = 0
 
@@ -34,13 +44,13 @@ dbgMode = 0
 #
 # MAIN
 #
-imgRGB = imread( imgFn )
+imgRGB = imread( args.infile )
 
 # Turn image into superpixels.
 spix = SuperPixels.computeSuperPixelGraph( imgRGB, 'slic', [400,10] )
 
 print 'Loading classifier...'
-clfr = bonzaClass.loadObject(clfrFn)
+clfr = bonzaClass.loadObject(args.clfrFn)
 
 print 'Computing superpixel features...'
 ftrs = FeatureGenerator.generateSuperPixelFeatures( imgRGB, spix.m_labels, [] )
@@ -49,30 +59,31 @@ print 'Computing class probabilities...'
 classProbs = bonzaClass.classProbsOfFeatures(ftrs,clfr,\
                                                  requireAllClasses=False)
 
-plt.interactive(1)
-plt.imshow(imgRGB)
-plt.title('original image')
-
-# show superpixels
-plt.figure()
-spix.draw()
-plt.title('Super Pixels')
-
-# show class labels per pixel
-classLabs = np.argmax( classProbs, 1 )
-# these are labs per region. turn into an image of labels.
-plt.figure()
-# +1 adds void class
-# todo: tidy this shemozzle up!
-pomio.showLabels( spix.imageFromSuperPixelData( classLabs + 1 ) )
-plt.title('Raw Classifier Labelling')
-plt.figure()
-pomio.showClassColours()
-
-plt.draw()
-if dointeract:
-    print 'Click plot to continue...'
-    plt.waitforbuttonpress()
+if args.verbose:
+    plt.interactive(1)
+    plt.imshow(imgRGB)
+    plt.title('original image')
+    
+    # show superpixels
+    plt.figure()
+    spix.draw()
+    plt.title('Super Pixels')
+    
+    # show class labels per pixel
+    classLabs = np.argmax( classProbs, 1 )
+    # these are labs per region. turn into an image of labels.
+    plt.figure()
+    # +1 adds void class
+    # todo: tidy this shemozzle up!
+    pomio.showLabels( spix.imageFromSuperPixelData( classLabs + 1 ) )
+    plt.title('Raw Classifier Labelling')
+    plt.figure()
+    pomio.showClassColours()
+    
+    plt.draw()
+    if dointeract:
+        print 'Click plot to continue...'
+        plt.waitforbuttonpress()
 
 #
 # Do the inference
@@ -82,24 +93,31 @@ if dointeract:
 nbrPotentialMethod = 'degreeSensitive'
 
 print 'Performing CRF inference...'
-plt.figure()
+if args.verbose:
+    plt.figure()
 
-for K in np.logspace(-2,0,5):
-    segResult = uflow.inferenceSuperPixel( \
-        spix,\
-        -np.log( np.maximum(1E-10, np.ascontiguousarray(classProbs) ) ), \
-        'abswap',\
-        nbrPotentialMethod, K )#, np.ascontiguousarray(nbrPotentialParams) )
+segResult = uflow.inferenceSuperPixel( \
+    spix,\
+    -np.log( np.maximum(1E-10, np.ascontiguousarray(classProbs) ) ), \
+    'abswap',\
+    nbrPotentialMethod, K )#, np.ascontiguousarray(nbrPotentialParams) )
+
+# turn from label classes to msrc labels
+segResult += 1
+print '   done.'
+
+if args.outfile and len(args.outfile)>0:
+    print 'Writing output label file %s' % args.outfile
+    outimg = pomio.msrc_convertLabelsToRGB( segResult )
+    skimage.io.imsave(args.outfile, outimg)
     print '   done.'
-    
-    # Show the result.
-    pomio.showLabels(segResult+1)
+
+# Show the result.
+if args.verbose:
+    pomio.showLabels(segResult)
     plt.title( 'Segmentation CRF result with K=%f' % K )
     plt.draw()
     print "labelling result, K = ", K 
     if dointeract:
         plt.waitforbuttonpress()
-    
-#plt.interactive(False)
-#plt.show()
 
