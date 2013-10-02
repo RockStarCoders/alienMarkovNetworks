856661607d43b28fad04c72ac6b2ac3af9dea0c1
amb-enthusiast <amb.enthusiast@gmail.com>
2013-10-02 01:30:40 -0600

    Fixed to work with predicted images and allow relative paths for prediction images and ground truth image files.  Example usage: python evalPredictions.py "/home/amb/dev/mrf/data/classifierResults/evalpairs.csv" "/home/amb/dev/mrf/data/MSRC_ObjCategImageDatabase_v2" "/home/amb/dev/mrf/data/classifierResults"

--- evalPredictions.py
+++ evalPredictions.py
@@ -6,15 +6,18 @@
 # discount any and all void labels in ground truth; count as incorrect if predicted
 # images are the same size :)
 # assume that the indexes align i.e. idx=1 refers to the same class
+# python evalPredictions.py "/home/amb/dev/mrf/data/classifierResults/evalpairs.csv" "/home/amb/dev/mrf/data/MSRC_ObjCategImageDatabase_v2"
 
 import pomio, FeatureGenerator, SuperPixels, SuperPixelClassifier
 
 import numpy as np
 
 import argparse
+
+from skimage import io
     
 
-def evaluateFromFile(evalFile, sourceData):
+def evaluateFromFile(evalFile, sourceData, predictDir):
     evalData = None
     
     evalData = pomio.readEvaluationListFromCsv(evalFile)
@@ -25,11 +28,13 @@ def evaluateFromFile(evalFile, sourceData):
     print "INFO: Source data = " + str(sourceData)
     print "\nINFO 1st element in eval result::" , evalData[0]
 
+    if predictDir.endswith("/") == False:
+        predictDir = predictDir + "/"
+
     results = []
 
     # for each eval pair (prediction labels and ground truth labels) do pixel count
     
-    predictDir = "/home/amb/dev/mrf/data/classifierResults"
     for idx in range(0, len(evalData)):
     
         print "\n\tINFO: Input eval data" , len(evalData[idx]) , "\n\t\t" , evalData[idx] , "\n\t\t" , evalData[idx][0] , "\n\t\t" , evalData[idx][1]
@@ -37,25 +42,21 @@ def evaluateFromFile(evalFile, sourceData):
         predictFile = evalData[idx][0]
         gtFile = evalData[idx][1]
         
-        print "\tINFO: predict file = " + str(predictFile)
-        print "\tINFO: ground truth = " + str(gtFile)
-        
         gt = loadReferenceGroundTruthLabels(sourceData, gtFile)
         
-        predict = loadPredictionImageLabels(predictDir + "/" + predictFile)
+        predict = loadPredictionImageLabels(predictDir + predictFile)
         
         result = evaluatePrediction(predict, gt)
-        results.append(result.append(gtFile))
+        results.append(result)
 
     # Results
-    print "Processed", len(results) , "evaluation results"
+    print "Processed total of ", len(results) , "predictions"
     for idx in range(0, len(results)):
-        print "\tResult#1: " , result[idx]
+        print "\tResult#" + str(idx+1) + ":" , results[idx]
     
     print "Processing complete."
 
 
-# functions to read predict and ground truth files
 def evaluatePrediction(predictLabels, gtLabels):
     print np.shape(predictLabels)
     print np.shape(gtLabels)
@@ -98,7 +99,6 @@ def evaluatePrediction(predictLabels, gtLabels):
     
     validGtPixels = allPixels - voidGtPixels
     
-    
     print "\tTotal pixels =\t" , allPixels
     print "\tVOID pixels  =\t" , voidGtPixels
     print "\tValid GT pixels =\t" , validGtPixels
@@ -112,14 +112,12 @@ def evaluatePrediction(predictLabels, gtLabels):
 
 def loadReferenceGroundTruthLabels(sourceData, imgName):
 
-    print "\tLoading ground truth labels from " + str(sourceData) + " directory"
     # should be only one image
     gtFile = str(sourceData) + "/GroundTruth/" + str(imgName)
     
     if "_GT" in imgName:
         imgName = imgName.replace("_GT" , "")
 
-    print "\tGT file = " + str(imgName)    
     gtImgLabels = pomio.msrc_loadImages(sourceData , ["Images/" + imgName] )[0].m_gt
 
     return gtImgLabels
@@ -127,37 +125,30 @@ def loadReferenceGroundTruthLabels(sourceData, imgName):
 
 
 def loadPredictionImageLabels(predictImgLabelsFile):
-    # use pomio to read in the labels file
-    print "\n\tPredicted labels @ " + str(predictImgLabelsFile)
-    
-    predictLabels = None
-    if predictImgLabelsFile.endswith(".csv"):
-        predictLabels = pomio.readMatFromCSV(predictImg)
-    elif predictImgLabelsFile.endswith(".pkl"):
-        predictLabels = pomio.unpickleObject(predictImgLabelsFile)
-    else:
-        print "Couldn't load" , predictImgLabelsFile
-    
-    assert predictLabels != None, "Exception trying to load prediction labels from " + str(predictImgLabelsFile)
+    # assume an image file, use pomio to convert
+    predictLabels = pomio.msrc_convertRGBToLabels( io.imread(predictImgLabelsFile) )
     
     return predictLabels
-
-
+    
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Evaluate predicted class labels against ground truth image labels')
     
     parser.add_argument('evalFile', type=str, action='store', \
-                            help='full filename of evaluation file which specifies a comma-separated list of predictions+ground truth pairs')
+                            help='CSV file listing predictions+ground truth pairs')
     parser.add_argument('sourceData', type=str, action='store', \
                             help='Path to source data directory for reference data (assumed to be same structure as MSRC data)')
+    parser.add_argument('predictData', type=str, action='store', \
+                            help='Parent path for relative filenames for predictions')
+                            
     args = parser.parse_args()
 
     evalFile = args.evalFile
     sourceData = args.sourceData
+    predictData = args.predictData
     
-    evaluateFromFile(evalFile, sourceData)
+    evaluateFromFile(evalFile, sourceData, predictData)
 
 
 
@@ -188,14 +179,5 @@ def test():
     #print "\tNow do a check of ground truth vs ground truth::" , evaluatePrediction(groundTruth, groundTruth)
     #print "\tNow do a check of prediction vs prediction::" , evaluatePrediction(prediction, prediction)
 
-
-def test2():
-    # Simple test to read in a evaluation file and display results
-    evalFile = "/home/amb/dev/mrf/eval/testEvalFile.csv"
-    msrcData = "/home/amb/dev/mrf/data/MSRC_ObjCategImageDatabase_v2"
-    
-    evalResult = evaluateFromFile(evalFile, msrcData)    
-    
-    print "Test2 evaluation result::\n" + str(evalResult)
     
     
